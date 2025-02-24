e631ca0ad378841bb4258b24919301aa4628f226
SdmSlavGit <200369058+sdmslavgit@users.noreply.github.com>
2025-02-24 23:56:54 +0100

    init

---
+++ background.js
@@ -0,0 +1,48 @@
+chrome.runtime.onInstalled.addListener(() => {
+  // Opcja dla Cambridge Dictionary
+  chrome.contextMenus.create({
+    id: "lookupCambridge",
+    title: "Szukaj w Cambridge Dictionary",
+    contexts: ["selection"]
+  });
+  
+  // Option for Collins Dictionary
+  chrome.contextMenus.create({
+    id: "lookupCollins",
+    title: "Search in Collins Dictionary",
+    contexts: ["selection"]
+  });
+
+
+  
+  // Opcja dla Forvo
+  chrome.contextMenus.create({
+    id: "lookupForvo",
+    title: "Search in Forvo",
+    contexts: ["selection"]
+  });
+
+  
+  
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  const selectedText = info.selectionText.trim();
+  if (selectedText.length > 0) {
+    let url = "";
+    if (info.menuItemId === "lookupCambridge") {
+      url = "https://dictionary.cambridge.org/dictionary/english/" + encodeURIComponent(selectedText);
+      url = url.replace(/%2B/g, "-");
+    } else if (info.menuItemId === "lookupCollins") {
+      url = "https://www.collinsdictionary.com/dictionary/english/" + encodeURIComponent(selectedText);
+      url = url.replace(/%2B/g, "-");
+      url = url.replace(/%20/g, "-");
+    } else if (info.menuItemId === "lookupForvo") {
+      url = "https://pl.forvo.com/search/" + encodeURIComponent(selectedText) + "/";
+    }
+    if (url) {
+      chrome.tabs.create({ url: url });
+    }
+  }
+});
+
